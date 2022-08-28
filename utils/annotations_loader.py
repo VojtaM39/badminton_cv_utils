2a7b61f1ea55ee2a279b11b2c9197c5f38d1330f
VojtaM39 <vojtech.maslan1@gmail.com>
2022-08-28 14:23:37 +0200

    Added annotations loader

---
+++ utils/annotations_loader.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+
+def load_annotations(annotation_file):
+  tree = ET.parse(DATA_ANNOTATION_FILE)
+  root = tree.getroot()
+
+  # Parse time slots
+  time_orders = dict()
+  for child in root:
+    if child.tag == 'TIME_ORDER':
+      for time_element in child:
+        time_orders[time_element.attrib['TIME_SLOT_ID']] = get_frame_from_milliseconds(int(time_element.attrib['TIME_VALUE']))
+  
+  annotations = []
+  for child in root:
+    if child.tag == 'TIER' and child.attrib['TIER_ID'] in ['fineanno', 'shotanno']:
+      for annotation in child:
+        # Check annotation value
+        annotation_val = annotation[0][0].text
+        if annotation_val in ANNOTATIONS_TO_KEEP_TOP or annotation_val in ANNOTATIONS_TO_KEEP_BOTTOM:
+          annotations.append(
+              {
+                'ref1': time_orders[annotation[0].attrib['TIME_SLOT_REF1']],
+                'ref2': time_orders[annotation[0].attrib['TIME_SLOT_REF2']],
+                'value': annotation_val
+              }
+          )
+
+
