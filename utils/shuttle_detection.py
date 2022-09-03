27de2f205fd2d4b331a1faf936b84dcbd10d0983
VojtaM39 <vojtech.maslan1@gmail.com>
2022-09-03 13:32:16 +0200

    Added utility for cropping detected court

---
+++ utils/shuttle_detection.py
@@ -0,0 +1,32 @@
+def get_court_rectangle(frame, corners, top_offset, bottom_offset, horizontal_offset):
+  """
+  Returns croped court from frame
+  """
+  # Get max and min for X and Y
+  max_x, max_y = float('-inf'), float('-inf')
+  min_x, min_y = float('inf'), float('inf')
+
+  for corner in corners:
+    x, y = corner
+
+    if x is None or y is None:
+      return None
+    
+    if x > max_x:
+      max_x = x
+      
+    if y > max_y:
+      max_y = y
+      
+    if y < min_x:
+      min_x = x
+      
+    if y < min_y:
+      min_y = y
+  
+  max_x += horizontal_offset
+  max_y += bottom_offset
+  min_x -= horizontal_offset
+  min_y -= top_offset
+
+  return np.array(frame[min_y:max_y,min_x:max_x,:]), (min_x, max_x, min_y, max_y)
